b3567bc00885aa3677b97317864c442133c4937c
Evgenia Martynova <evg.martynova@gmail.com>
2019-12-08 23:21:04 +0100

    Take countries of samples into account when split for double cross-validation

--- preprocess/crossValidation.m
+++ preprocess/crossValidation.m
@@ -2,21 +2,49 @@
 
 load('oils.mat')
 samples = Data.samples;
+countries = Data.countries;
+values = Data.values;
 
-[validation, test] = splitData(samples);
+[validation, test] = splitData(samples, countries);
 
-% 10 fold validation
-SplitsNumber = 10;
-indices = crossvalind('Kfold',validation,SplitsNumber);
+%find minimum by country
+N = length(validation);
+min = length(validation{1});
+for i = 2:N
+    if length(validation{i}) < min
+        min = length(validation{i});
+    end
+end
 
+% to have at least one item by country
+SplitsNumber = min;
+indices = {};
+% by country
+for i = 1:N
+    current_country_val = validation{i};
+    indices{i} = crossvalind('Kfold',current_country_val,SplitsNumber);
+end
+
+% cross ffol validation is in this loop
 for foldIndex = 1:SplitsNumber
-    test = (indices == foldIndex);
-    train =~ test;
-    trainSampleNumbers = validation(train,:);
+    % by country
+    trainSampleNumbers = [];
+    testSampleNumbers = [];
+    for i = 1:N
+        test1 = (indices{i} == foldIndex);
+        train1 =~ test1;
+        testSampleNumbers1 = validation{i}(test1,:);
+        testSampleNumbers = [testSampleNumbers; testSampleNumbers1];
+        trainSampleNumbers1 = validation{i}(train1,:);
+        trainSampleNumbers = [trainSampleNumbers; trainSampleNumbers1];
+    end
+
     trainSamples = getAllSamplesIndexes(samples, trainSampleNumbers);
+    trainValues = values(trainSamples,:);
+    trainCountries = countries(trainSamples);
     
-    testSampleNumbers = validation(test,:);
     testSamples = getAllSamplesIndexes(samples, testSampleNumbers);
+    testValues = values(testSamples);
     % evaluate the performance of classifier
 end
 
--- preprocess/splitData.m
+++ preprocess/splitData.m
@@ -1,4 +1,4 @@
-function [validationSet, testSet] = splitData(samples, testSplit)
+function [validationSet, testSet] = splitData(samples, countries, testSplit)
 %Splits data into validation and idependent tests set
 %   samples - array of samples with duplicated samples
 %   testSplit - the percentage of the samples to leave in the tests set
@@ -8,15 +8,26 @@ function [validationSet, testSet] = splitData(samples, testSplit)
 %   validationSet - array of unique ids of samples to use as validation set
 %   testSet - array of unique ids of samples to use as test set
 
-if nargin<2
+if nargin<3
   testSplit = 0.25;
 end
 
-uniqueSamples = unique(samples);
-shuffledUniqueSamples = uniqueSamples(randperm(length(uniqueSamples)));
-divisionBoundary = round(length(uniqueSamples)*(1-testSplit));
-validationSamples = shuffledUniqueSamples(1:divisionBoundary);
-testSamples = shuffledUniqueSamples(divisionBoundary+1:end);
+% we split data by country to have the same percentage of samples from
+% different countrues in both test and training data sets
+validationSamples = {};
+testSamples = {};
+countriesCodes = unique(countries);
+
+for i=1:length(countriesCodes)
+    code = countriesCodes(i);
+    neededCountry = (countries == code);
+    countrySammples = samples(neededCountry,:);
+    uniqueSamples = unique(countrySammples);
+    shuffledUniqueSamples = uniqueSamples(randperm(length(uniqueSamples)));
+    divisionBoundary = round(length(uniqueSamples)*(1-testSplit));
+    validationSamples{i} = shuffledUniqueSamples(1:divisionBoundary);
+    testSamples{i} = shuffledUniqueSamples(divisionBoundary+1:end);
+end
 
 validationSet = validationSamples;
 testSet = testSamples;
