bcff9f22c27b537c6fa374116620989d7471a88e
Evgenia Martynova <evg.martynova@gmail.com>
2020-01-08 11:46:44 +0100

    Final GA results evaluation

--- implementation/GA/lda_with_ga_results.m
+++ implementation/GA/lda_with_ga_results.m
@@ -8,47 +8,53 @@ wavelengths = Data.wavelength;
 
 features_sets = load('features.txt', '-ASCII');
 
-folds = kFoldSplit(samples, countries);
-folds_num = length(folds);
-
 % zero loo cv error solutions number
 D = 24;
+% runs to evaluate solutions
+M = 10;
+solution_mean_errors = zeros(M, D);
 
-solution_mean_errors = zeros(1, D);
-for d=1:D
-    features = features_sets(d,:);
-    error_rates = zeros(1, folds_num);
-    for k=1:folds_num
-        validation = folds{k}.train;
-        test = folds{k}.test;
+for m=1:M
+    fprintf('Run %.4f \n ', m);
+    folds = kFoldSplit(samples, countries);
+    folds_num = length(folds);
+    for d=1:D
+        features = features_sets(d,:);
+        error_rates = zeros(1, folds_num);
+        for k=1:folds_num
+            validation = folds{k}.train;
+            test = folds{k}.test;
+            
+            % training data set
+            trainSamples = getAllSamplesIndexes(samples, validation);
+            trainValues = values(trainSamples,:);
+            trainCountries = countries(trainSamples);
+            
+            % idependent test set
+            testSamples = getAllSamplesIndexes(samples, test);
+            testValues = values(testSamples,:);
+            testCountries = countries(testSamples);
+            
+            trainValues1 = trainValues(:,features);
+            testValues1 = testValues(:,features);
+            
+            % pre-processing (mean-centering)
+            [trainValues1, trainMeans] = mncn2(trainValues1);
+            % apply means calculated for training set to the tets set
+            testValues1 = mncn2(testValues1, trainMeans);
+            
+            [ldaclass,err,p,logp,coeff]=classify(testValues1,trainValues1,trainCountries,'linear');
+            error_rates(k) = 100*sum(ldaclass~=testCountries)/size(testCountries,1);
+        end
         
-        % training data set
-        trainSamples = getAllSamplesIndexes(samples, validation);
-        trainValues = values(trainSamples,:);
-        trainCountries = countries(trainSamples);
-        
-        % idependent test set
-        testSamples = getAllSamplesIndexes(samples, test);
-        testValues = values(testSamples,:);
-        testCountries = countries(testSamples);
-        
-        trainValues1 = trainValues(:,features);
-        testValues1 = testValues(:,features);
-        
-        % pre-processing (mean-centering)
-        [trainValues1, trainMeans] = mncn2(trainValues1);
-        % apply means calculated for training set to the tets set
-        testValues1 = mncn2(testValues1, trainMeans);
-        
-        [ldaclass,err,p,logp,coeff]=classify(testValues1,trainValues1,trainCountries,'linear');
-        error_rates(k) = 100*sum(ldaclass~=testCountries)/size(testCountries,1);
+        solution_mean_errors(m, d) = mean(error_rates);
+        fprintf('GA solution %.4f: 4-fold CV average error %.4f ', d, mean(error_rates));
+        fprintf('\n');
     end
-    
-    solution_mean_errors(d) = mean(error_rates);
-    fprintf('GA solution %.4f: 4-fold CV average error %.4f ', d, mean(error_rates));
-    fprintf('\n');
 end
 
-fprintf('Mean top GA solutions 4-fold CV average error %.4f ', mean(solution_mean_errors));
-fprintf('\n');
+means = mean(solution_mean_errors);
+
+fprintf('Mean top GA solutions 4-fold CV average error \n');
+disp(means)
 
